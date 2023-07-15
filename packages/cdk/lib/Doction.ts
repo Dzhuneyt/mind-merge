20c834ee8ebbf6079fb8a73759d8c6c74942a984
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2023-07-15 18:59:57 +0300

    feat: Init AppSync

--- packages/cdk/lib/Doction.ts
+++ packages/cdk/lib/Doction.ts
@@ -1,14 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
+import {CfnOutput} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {UserPool} from "aws-cdk-lib/aws-cognito";
-import {CfnOutput} from "aws-cdk-lib";
+import {UserPool, UserPoolEmail} from "aws-cdk-lib/aws-cognito";
 import {IdentityPool, UserPoolAuthenticationProvider} from "@aws-cdk/aws-cognito-identitypool-alpha";
-import {UserPoolEmail} from "aws-cdk-lib/aws-cognito";
+import {FieldLogLevel, GraphqlApi} from "aws-cdk-lib/aws-appsync";
+import {RetentionDays} from "aws-cdk-lib/aws-logs";
+import {CodeFirstSchema, GraphqlType, ObjectType} from "awscdk-appsync-utils";
 
 export class Doction extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        this.createUserPool();
+        this.createAppSyncApi();
+    }
+
+    private createUserPool() {
         const userPool = new UserPool(this, 'UserPool', {
             selfSignUpEnabled: true,
             removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -39,5 +46,27 @@ export class Doction extends cdk.Stack {
         new CfnOutput(userPoolClient, 'UserPoolClientId', {
             value: userPoolClient.userPoolClientId,
         })
+    }
+
+    private createAppSyncApi() {
+        const schema = new CodeFirstSchema()
+
+        schema.addType(new ObjectType('demo', {
+            definition: {id: GraphqlType.id()},
+        }));
+        schema.addType(new ObjectType('Query', {
+            definition: {},
+        }));
+
+        new GraphqlApi(this, 'api', {
+            authorizationConfig: {},
+            name: 'GraphqlApi',
+            schema,
+            logConfig: {
+                fieldLogLevel: FieldLogLevel.ALL,
+                retention: RetentionDays.ONE_WEEK,
+                excludeVerboseContent: true,
+            },
+        });
     }
 }
